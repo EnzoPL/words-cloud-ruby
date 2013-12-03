7e92af79f408565d2d02a6f48392e4bc014e08cc
Hugo <hugo.vauvert@y-nov.com>
2013-12-03 15:45:56 +0100

    exo presque fini

--- cycle6.rb
+++ cycle6.rb
@@ -1,17 +1,18 @@
-# encoding: utf-8
+﻿# encoding: utf-8
 
 =begin rdoc
-= Cycle 5 : version mobile
+= Cycle 6 : saisie des urls et nuage des mots
 
 Serveur web APPLI
- 
- usage: ruby cycle5.rb
+
+ usage: ruby cycle6.rb
 Lance un serveur ruby webrick accessible
 à l'url http://localhost:5555/
 =end
 
 require "webrick"
 require "net/http"
+require "./nuage"
 
 =begin rdoc
 = Serveur APPLI
@@ -20,13 +21,13 @@ require "net/http"
  - Répond aux urls : /, /urls et /mobile/urls
 =end
 class APPLI
-  VERSION = '5.0'
+  VERSION = '1.0'
 
-# 
-# Exemple : 
-#	appli = APPLI.new()
-#	port = 1234
-#	appli = APPLI.new(port)
+#
+# Exemple :
+#       appli = APPLI.new()
+#       port = 1234
+#       appli = APPLI.new(port)
   def initialize(port=5555)
    @webrick = WEBrick::HTTPServer.new(
      :Port         => port,
@@ -38,17 +39,20 @@ class APPLI
         [ $stderr, WEBrick::AccessLog::AGENT_LOG_FORMAT   ],
       ]
     )
+    @urls = {}
     # urls connues par défaut
-    @urls = {"http://algec.iut-blagnac.fr/~jmi/rubym2igs" => Time.now,
-    "http://www.ingesup.com/ecole-informatique/toulouse.html" => Time.now,
-    "http://ruby-doc.org/" => Time.now,
-    "http://ruby-doc.org/docs/ProgrammingRuby/" => Time.now
-    }
-    
+   ['http://www.ingesup.com/ecole-informatique/toulouse.html',
+   'http://www.bibliotheque.toulouse.fr/accueil_mediatheque.html',
+   'http://csrp.iut-blagnac.fr/~jmi/rubym2igs/',
+   'http://coin.des.experts.pagesperso-orange.fr/reponses/faq9_56.html',
+   'http://ruby-doc.org/docs/beginner-fr/xhtml/'].each do |url|
+           @urls[url] = Url.new(url)
+    end
+
   end
-  
+
  # Monte les urls  : / et /urls
- # Démarre le serveur (webrick) 
+ # Démarre le serveur (webrick)
   def start
       begin
       @webrick.mount("/", APPLIWI)
@@ -66,7 +70,7 @@ end
 
 # Interface HTTP du serveur APPLI pour /
 class APPLIWI < WEBrick::HTTPServlet::AbstractServlet
-	
+
 =begin rdoc
 - Redirige le navigateur vers /urls pour toute requête sur /
 =end
@@ -74,20 +78,19 @@ class APPLIWI < WEBrick::HTTPServlet::AbstractServlet
     res.set_redirect(WEBrick::HTTPStatus::Found, "/mobile/urls") if req.path =~ /^\/mobile/
     res.set_redirect(WEBrick::HTTPStatus::Found, "/urls")
   end
-  
+
   alias do_POST do_GET
-  
+
 end # class
 
 # Interface HTTP du serveur APPLI pour /urls
 class APPLIWIURLS < WEBrick::HTTPServlet::AbstractServlet
-  MOIS=['oups','Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
-  
+  MOIS=['oups','Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre']
 
   def initialize(config,urls)
-	  super
-	  @urls = urls
-	  @nbmots = 10
+          super
+          @urls = urls
+          @nbmots = 10
   end
 
 =begin rdoc
@@ -97,61 +100,40 @@ class APPLIWIURLS < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(req, res)
     res["content-type"] = "text/html"
     res.body = <<-EOF
-    <html><head>
-     <meta http-equiv='Content-Type' content='text/html'; charset=UTF-8'>
-     <title>Serveur APPLI - v#{APPLI::VERSION}</title>
-     </head><body><hr/>
-     <center><h2>Hello web</h2></center>
-     <form method="POST">
-     <input type="text" name="url" size="80" value="Nouvelle url" />
-     <input type="submit" value="Enregistrer">
-     </form>
-     <hr/>
-     <ol>
+<html><head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+<title>Serveur APPLI - v#{APPLI::VERSION}</title>
+#{Nuage.do_style}
+</head><body><hr/>
+<center><h2>Hello web</h2></center>
+<form method="POST">
+<input type="text" name="url" size="80" value="Nouvelle url" />
+<input type="submit" value="Enregistrer">
+</form>
+<hr/>
+<ol>
 EOF
-@urls2 = [['www.ingesup.com',80,'/ecole-informatique/toulouse.html'],['www.bibliotheque.toulouse.fr',80,'/accueil_mediatheque.html'],['csrp.iut-blagnac.fr',80,'/~jmi/rubym2igs/'],['coin.des.experts.pagesperso-orange.fr',80,'/reponses/faq9_56.html'],['ruby-doc.org',80,'/docs/beginner-fr/xhtml/']]
-@urls2.each {|host,port,path|
-	 http = Net::HTTP.new(host, port)
-	 reponse = http.get(path)
-	 encoding = nil
-	 encoding = $1 if reponse.header['content-type'] =~ /charset=(.*)/i
-	 encoding = $1 if not encoding and reponse.body =~ /<meta [^<]+charset=([^"']+)/i
-	 encoding = 'ISO-8859-1' if not encoding
 
-	 reponse.body.force_encoding(encoding.upcase) if encoding
-	 reponse.body.encode!('utf-8')
-	 
-	 reponse.body.gsub!(/.*<body/m,' ')
-	 reponse.body.gsub!(/<\/?[^>]*>/,' ')
-	 reponse.body.gsub!(/&[^;]+;/,' ')
-	 reponse.body.tr!("àçéèêîôù","aceeeiou")
-	 #p reponse.body
-	 words = reponse.body.downcase.scan(/[a-z]+/)
-	 p "#{host}#{path} : #{encoding} encoding, #{words.size} mots"
-	 #p words
-	 res.body <<= <<-EOF
-		<li>#{words}</li>
-		EOF
-}
-
-  @urls.each { |url,date|
+  @urls.each { |url,o|
    res.body <<= <<-EOF
-     <li><a href="#{url}">#{url}</a> - #{date.day} #{MOIS[date.mon]} #{date.year} #{date.strftime(" - %T")}
-     <form method="POST">
-     <input type="hidden" name="url" value="#{url}" />
-     <input type="submit" name="delete" value="Supprimer">
-     </form>
-     </li>
+<li><a href="#{o.name}">#{o.name}</a> - #{o.time.day} #{MOIS[o.time.mon]} #{o.time.year} #{o.time.strftime("- %T")}
+<form method="POST">
+<input type="hidden" name="url" value="#{o.name}" />
+<input type="submit" name="delete" value="Supprimer">
+</form>
+     #{o.nuage.mots_frequences.size} mots différents sur #{o.nuage.total_mots} mots dans la page
+     #{o.nuage.do_div}
+</li>
 EOF
    }
     res.body <<= <<-EOF
-     </ol>
-     <hr/>
-     <div style="text-align:right;">Serveur APPLI - v#{APPLI::VERSION}</div>
-     <hr/></body></html>
+</ol>
+<hr/>
+<div style="text-align:right;">Serveur APPLI - v#{APPLI::VERSION}</div>
+<hr/></body></html>
 EOF
   end
-  
+
 =begin rdoc
 - Supprime l'url fournie si delete est dans la requête
 - Enregistre l'url fournie dans @urls
@@ -163,13 +145,13 @@ EOF
 
     if req.query['delete'] && req.query['url'] != ""
     then
-	    @urls.delete(req.query['url'])
+            @urls.delete(req.query['url'])
     elsif req.query['url'] && req.query['url'] != ""
-	    @urls[req.query['url']] = Time.now
+            @urls[req.query['url']] = Url.new(req.query['url'])
     end
     res.set_redirect(WEBrick::HTTPStatus::Found, "")
   end
-  
+
 end # class
 
 class APPLIWIURLS_M < APPLIWIURLS
@@ -182,38 +164,39 @@ class APPLIWIURLS_M < APPLIWIURLS
     res["content-type"] = "text/html"
     res.body = <<-EOF
 <!DOCTYPE html>
-<html>
-<head>
-<title>My Page</title>
+<html><head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+<title>Serveur APPLI - v#{APPLI::VERSION}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
 <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
-</head>
-<body>
+ #{Nuage.do_style}
+</head><body>
 <div data-role="page">
 <div data-role="header">
 <h1>Hello web</h1>
 </div>
 <div data-role="content">
-     <form method="POST">
-     <input type="text" name="url" size="80" value="Nouvelle url" />
-     <input type="submit" value="Enregistrer">
-     </form>
+<form method="POST">
+<input type="text" name="url" size="80" value="Nouvelle url" />
+<input type="submit" value="Enregistrer">
+</form>
 <ol data-role="listview" data-inset="true">
 <li data-role="list-divider">urls</li>
 EOF
-   
+
   num = 0
-  @urls.each { |url,date|
+  @urls.each { |url,o|
    res.body <<= <<-EOF
-     <li><a href="#{url}">#{url}</a>
-     <form method="POST">
-     <input type="hidden" name="url" value="#{url}" />
-     <input type="submit" name="delete" value="Supprimer" data-inline="true">
-     </form>
-     <a href="#u#{num += 1}" ></a>
-     </li>
+<li><a href="#{url}">#{url}</a>
+<form method="POST">
+<input type="hidden" name="url" value="#{url}" />
+<input type="submit" name="delete" value="Supprimer" data-inline="true">
+<span>#{o.time.day} #{MOIS[o.time.mon]} #{o.time.year} #{o.time.strftime(" - %T")}</span>
+</form>
+<a href="#u#{num += 1}" ></a>
+</li>
 EOF
    }
     res.body <<= <<-EOF
@@ -221,9 +204,9 @@ EOF
 </div>
 </div><!-- /page -->
 EOF
-   
+
   num = 0
-  @urls.each { |url,date|
+  @urls.each { |url,o|
    res.body <<= <<-EOF
 <div data-role="page" id="u#{num += 1}"  data-add-back-btn="true">
 <div data-role="header">
@@ -232,7 +215,11 @@ EOF
 
 <div data-role="content">
 <center>
-#{date.day} #{MOIS[date.mon]} #{date.year} #{date.strftime(" - %T")}
+#{o.time.day} #{MOIS[o.time.mon]} #{o.time.year} #{o.time.strftime(" - %T")}
+</center>
+ #{o.nuage.mots_frequences.size} mots différents sur #{o.nuage.total_mots} mots dans la page
+<center>
+ #{o.nuage.do_div}
 </center>
 </div><!-- /content -->
 
@@ -243,19 +230,18 @@ EOF
 EOF
    }
     res.body <<= <<-EOF
-</body>
-</html>
+</body></html>
 EOF
   end
 end # class APPLIWIURLS_M
 
 # lance le serveur si le programme courant est ce fichier
 if $0 == __FILE__ then
-	port = ((ARGV.size > 0) and ARGV[0].to_i) || 5555
-	appli = APPLI.new(port)
-	trap(:INT){ 
-	 puts "Arret utilisateur" 
-	 appli.stop 
-	}
-	appli.start
+        port = ((ARGV.size > 0) and ARGV[0].to_i) || 5555
+        appli = APPLI.new(port)
+        trap(:INT){
+         puts "Arret utilisateur"
+         appli.stop
+        }
+        appli.start
 end
---
+++ nuage.rb
@@ -0,0 +1,137 @@
+﻿# encoding: utf-8
+
+require 'uri'
+require 'net/http'
+require 'singleton'
+require 'observer'
+
+=begin rdoc
+= Classe Urls
+
+ - Gère une url et son nuage associé
+=end
+class Url
+
+  attr_reader :name, :time, :nuage
+
+  def initialize(url)
+    @name = url
+    @time = Time.now
+    @nuage = Nuage.new(get_content)
+    @lastmodified = nil
+  end
+
+  def nuage=(content)
+    @nuage = Nuage.new(content)
+  end
+
+  def get_content
+    begin
+     proto,user,host,port,reg,path,opaq,query,fragment = URI.split(@name)
+     if proto == 'http' then
+       # GET page à traiter
+       port = port || 80
+       path = '/' if path == ''
+       http = Net::HTTP.new(host, port)
+       reponse = http.get(path)
+       #reponse.header.each_header {|key,value| puts "#{key} = #{value}" }
+       @lastmodified = reponse.header['last-modified'] if reponse.header['last-modified']
+       encoding = nil
+       encoding = $1 if reponse.header['content-type'] =~ /charset=(.*)/i
+       encoding = $1 if not encoding and reponse.body =~ /<meta [^<]+charset=([^"']+)/i
+ encoding = 'ISO-8859-1' if not encoding
+       reponse.body.force_encoding(encoding) if encoding
+       reponse.body.encode('utf-8')
+     end
+    rescue
+     p $!
+     nil
+    end
+  end
+
+end
+
+=begin rdoc
+= Classe Nuage
+
+ - Produit un nuage des mots les plus fréquents
+=end
+class Nuage
+  @@font_max = 5
+
+  NOISE_WORDS = {
+      :fr => ['a', "ai", "au", "ait", "c", "ca", "car", "ce", "ces", "cela", "celle", "cette", "ces", "d", "dans", "de", "du", "des", "dont", "en", "elle", "es", "est", "et", "il", "ils", "j", "je", "l", "le", "la", "les", "m", "ma", "me", "mes", "mon", "n", "ne", "ni", "ou", "pas", "plus", "pour", "qu", "que", "qui", "quel", "quelles", "s", "sa", "se", "ses", "son", "t", "ta", "te", "tes", "ton", "un", "une", "va", "y", "assert","class","def","end","equal","new","x"],
+      :en => ['a','an','are','and','be','border','s','lt','font','for','gt','is','in','left','margin','ma','mes','mon','n','nbsp','of','our','padding','par','pour','r','top','to','the','this','we','which','while','8217','5em','0','1','2']
+      }
+
+  def initialize(content, lang=:fr, nbmots=20, font_max=5)
+    @content = content
+    @nbmots = nbmots
+    @font_max = font_max
+    @lang = lang
+    @total_mots = 0
+    @mots_frequences = frequences
+  end
+
+  attr_reader :mots_frequences, :total_mots
+
+  def frequences
+    mot_freq = Hash.new(0)
+    if @content then
+      @content.gsub!(/.*<body/m,' ')
+      @content.gsub!(/<\/?[^>]*>/,' ')
+      @content.gsub!(/&[^;]+;/,' ')
+      @content.tr!("àçéèêîôù","aceeeiou")
+      words = @content.downcase.scan(/[a-z0-9]+/)
+      @total_mots = words.length
+      words.each {|word| mot_freq[word] += 1 unless NOISE_WORDS[@lang].index(word) }
+    end
+    mot_freq
+  end
+
+  def normalize
+    if @nbmots <= @mots_frequences.size
+     mft = @mots_frequences.sort{|a,b| a[1] <=> b[1]}[-(@nbmots), @nbmots]
+    else
+     mft = @mots_frequences.sort{|a,b| a[1] <=> b[1]}
+    end
+    p "#{@nbmots} ****** #{@mots_frequences.size}"
+    max = @mots_frequences.sort{|a,b| a[1] <=> b[1]}[-1][1]
+    max = mft.sort{|a,b| a[1] <=> b[1]}[-1][1]
+    mft.sort{|a,b| a[0] <=> b[0]}.collect { |k,v|
+      [k, v, ((v * (@font_max - 1))/max.to_f).floor + 1]
+    }
+  end
+
+  def do_div
+    div = "<div class='nuage'>\n"
+    (normalize.sort{|a,b| a[0] <=> b[0]}).each { |m,f,t|
+      div <<= "<span class='n#{t}'><a href='#' title='#{f}'>#{m}<sub>#{f}</sub></a></span>\n"
+    }
+    div <<= "</div>\n"
+  end
+
+  def self.do_style
+      style = "<style type='text/css'>\n"
+      (1..@@font_max).each do |i|
+         style <<= ".n#{i}{font-size:#{(i*0.2)+1}em;position:relative;z-index:#{@@font_max - i}}\n"
+      end
+      style <<= ".nuage {line-height:2.4em;max-width:50%;text-align:justify;margin:1em 1em 0 1em;}\n"
+      style <<= ".nuage a:link{text-decoration:none;}\n"
+      style <<= ".nuage a:hover{background-color:silver;}\n"
+      style <<= ".nuage a:visited{text-decoration:none;}\n"
+      style <<= "sub {color:black;}"
+      style <<= "</style>\n"
+  end
+
+end #class
+
+
+# calcule des nuages si le programme courant est ce fichier
+if $0 == __FILE__ then
+    ['http://www.ingesup.com/ecole-informatique/toulouse.html','http://www.bibliotheque.toulouse.fr/accueil_mediatheque.html','http://csrp.iut-blagnac.fr/~jmi/rubym2igs/','http://coin.des.experts.pagesperso-orange.fr/reponses/faq9_56.html','http://ruby-doc.org/docs/beginner-fr/xhtml/'].each { |url|
+     u = Url.new(url)
+     p u.nuage.mots_frequences
+     p u.nuage.do_div
+    }
+end
